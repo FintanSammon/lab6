2eebd5c96f8bc9cc25eed85c5f38a0b7e7d5ae6a
FintanSammon <72512712+FintanSammon@users.noreply.github.com>
2023-11-12 14:49:33 +0000

    Update bookItem.js

--- src/components/bookItem.js
+++ src/components/bookItem.js
@@ -1,16 +1,21 @@
-//For bookItem it displays 
+// Importing the 'Card' component from 'react-bootstrap'
 import Card from 'react-bootstrap/Card';
+
+// Defining a functional React component named 'BookItem' that takes 'props' as its parameter
 function BookItem(props) {
   return (
     <div>
-
-
+      {/* Creating a Bootstrap Card component to display book information */}
       <Card>
+        {/* Displaying the book's title as the Card header */}
         <Card.Header>{props.mybook.title}</Card.Header>
         <Card.Body>
           <blockquote className="blockquote mb-0">
-            <img src={props.mybook.thumbnailUrl}></img>
+            {/* Displaying the book's thumbnail image */}
+            <img src={props.mybook.thumbnailUrl} alt="Book Thumbnail" />
+
             <footer>
+              {/* Displaying the first author's name */}
               {props.mybook.authors[0]}
             </footer>
           </blockquote>
@@ -20,4 +25,5 @@ function BookItem(props) {
   );
 }
 
+// Exporting the 'BookItem' component so it can be used in other parts of the application
 export default BookItem;
