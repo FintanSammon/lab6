6a8b2953870336d37268ddb24e16ba3fa385d5b8
FintanSammon <72512712+FintanSammon@users.noreply.github.com>
2023-11-12 14:38:33 +0000

    Update read.js

--- src/components/read.js
+++ src/components/read.js
@@ -3,32 +3,34 @@ import Books from "./books";
 import axios from "axios";
 
 function Read() {
-
-
+    // Defining state variable 'data' to store book data received from the server
     const [data, setData] = useState([]);
-//use effect is a react hook that allows 
+
+    // Using the useEffect hook to fetch data from the server when the component mounts
     useEffect(() => {
-        //Axious
+        // Sending a GET request to the server to retrieve a list of books
         axios.get('http://localhost:4000/api/books')
             .then(
                 (response) => {
+                    // Updating the 'data' state with the book data from the response
                     setData(response.data.myBooks);
                 }
             )
             .catch(
                 (error) => {
+                    // Handle errors if the GET request fails
                     console.log(error);
                 }
             );
-    }, []);
+    }, []); // The empty dependency array ensures that this effect runs only once when the component mounts
 
     return (
         <div>
             <h2>Hello from my Read Component</h2>
+            {/* Render the 'Books' component and pass the 'data' state as a prop */}
             <Books myBooks={data}></Books>
         </div>
     );
-
 }
 
 export default Read;
